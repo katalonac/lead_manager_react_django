35230fdce4dd1613fee64b08d4798e8b821929fb
Kumovic Nenad <nenad.kumovic@it4communication.ch>
2019-12-06 20:23:59 +0100

    Implementing Django Token Authentication

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-import { createMessage } from './messages';
+import { createMessage, returnErrors } from './messages';
 
-import { GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERRORS } from './types';
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from './types';
 
 axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
 axios.defaults.xsrfCookieName = "csrftoken";
@@ -14,8 +14,11 @@ export const getLeads = () => dispatch => {
         type: GET_LEADS,
         payload: res.data
       });
-    }).catch(err => console.log(err))
-}
+    }).catch(err => dispatch(
+      returnErrors(
+        err.response.data, 
+        err.response.status)));
+};
 
 // DELETE LEAD
 export const deleteLead = (id) => dispatch => {
@@ -27,7 +30,7 @@ export const deleteLead = (id) => dispatch => {
         payload: id
       });
     }).catch(err => console.log(err))
-}
+};
 
 // ADD LEAD
 export const addLead = (lead) => dispatch => {
@@ -38,14 +41,8 @@ export const addLead = (lead) => dispatch => {
         type: ADD_LEAD,
         payload: res.data
       });
-    }).catch(err => {
-      const errors = {
-        msg: err.response.data,
-        status: err.response.status
-      }
-      dispatch({
-        type: GET_ERRORS,
-        payload: errors
-      });
-    });
-}
+    }).catch(err => dispatch(
+      returnErrors(
+        err.response.data, 
+        err.response.status)));
+};
--- leadmanager/leads/api.py
+++ leadmanager/leads/api.py
@@ -5,8 +5,11 @@ from .serializers import LeadSerializer
 
 # Lead ViewSet
 class LeadViewSet(viewsets.ModelViewSet):
-    queryset = Lead.objects.all()
-    permission_classes = [
-        permissions.AllowAny
-    ]
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = LeadSerializer
+
+    def get_queryset(self):
+        return self.request.user.leads.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
