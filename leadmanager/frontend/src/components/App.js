a2c55386231c88be38531dcd5bc66c7b3e660b68
Kumovic Nenad <nenad.kumovic@it4communication.ch>
2019-12-07 13:08:10 +0100

    Create Auth State & Private Routes

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,12 +1,17 @@
 import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 
 import Header from './layout/Header';
 import Dashboard from './leads/Dashboard';
-import Alerts from './layout/Alerts'
+import Alerts from './layout/Alerts';
+import Login from './accounts/Login';
+import Register from './accounts/Register';
+import PrivateRoute from './common/PrivateRoute';
 
 import { Provider } from 'react-redux';
 import store from '../store';
+import { loadUser } from '../actions/auth';
 
 import { Provider as AlertProvider } from 'react-alert';
 import AlertTemplate from 'react-alert-template-basic';
@@ -18,17 +23,28 @@ const alertOptions = {
 };
 
 class App extends Component {
+  // Fired up when app loads
+  componentDidMount() {
+    store.dispatch(loadUser());
+  }
+
   render() {
     return (
       <Provider store={store}>
         <AlertProvider template={AlertTemplate} {...alertOptions}>
+          <Router>
           <Fragment>
             <Header />
             <Alerts />
             <div className="container">
-              <Dashboard />
+              <Switch>
+                <PrivateRoute exact path="/" component={Dashboard} />
+                <Route exact path="/register" component={Register} />
+                <Route exact path="/login" component={Login} />
+              </Switch>
             </div>
           </Fragment>
+          </Router>
         </AlertProvider>
       </Provider>
     )
